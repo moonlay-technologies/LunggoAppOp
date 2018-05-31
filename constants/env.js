08d659f004a8c1edbd927abcf191dce37ecc7d98
Waski <indera.aji@travelmadezy.com>
2018-05-31 10:00:02 +0700

    make client id secret and url change according to release channel

--- constants/env.js
+++ constants/env.js
@@ -1,6 +1,63 @@
-export const clientId = "V1ZoQ2RXTjZiM2xNYWtGMVRVUnZlVTVFUm14T1JFVTBXbGRWZVU5RWJHcE9WRUY2VGpKYWFsbDZVVFZhYlVVeVQxUk5NVmxxVlhwUFYwcHNXVzFWZUZwcVozbz0=";
-export const clientSecret = "V1RKSk1sa3lUWGxOUkdOM1dYcEdhMDB5VW1wT2VrMDFUWHBPYTA5RVNUVlBWRmsxVGtkYWEwMVViRzFhYlVsNVdWUkNhZz09";
+import { Platform } from 'react-native';
+
+const channel = Expo.Constants.manifest.releaseChannel;
+
 export const deviceId = Expo.Constants.deviceId;
-export const AUTH_LEVEL = { Guest: '1', User: '2' }
-export const DOMAIN = 'http://travorama-local-cw.azurewebsites.net';
-export const API_DOMAIN = 'http://travorama-local-api.azurewebsites.net';
+export const AUTH_LEVEL = { Guest: '1', User: '2' };
+export const WEB_DOMAIN = (() => {
+    switch (channel) {
+        case "production":
+            return 'https://www.travorama.com';
+        case "qa":
+            return 'https://travorama-qa-cw.azurewebsites.net';
+        default:
+            return 'https://travorama-local-cw.azurewebsites.net';
+    }
+})();
+export const API_DOMAIN = (() => {
+    switch (channel) {
+        case "production":
+            return 'https://api.travorama.com';
+        case "qa":
+            return 'https://travorama-qa-api.azurewebsites.net';
+        default:
+            return 'https://travorama-local-api.azurewebsites.net';
+    }
+})();
+
+const appVersion = Expo.Constants.manifest.version;
+const appType = Expo.Constants.appOwnership;
+const platform = Platform.OS;
+const clientGuide = "Please set a new client ID and secret for every new version. Set it in env.js on app and in Client table on server database.\n\nTo create client ID, create string in this format \"{platformCode}:{version}:{random string}\" (without quote) then encode it in base64 3 (three) times. Set platformCode as \"rnandroidop\" for android and \"rniosop\" for ios. Set the resulting client ID in env.js on app according to version and in Client table on server database.\n\nTo create Client Secret, create a random string and set it in env.js on app according to version code, then hash it with SHA512 and set it in Client table on server database."
+
+// You're free to clean up old client ID and secret as we can retrieve it back from old commits.
+
+export const clientId = (() => {
+    // DEV
+    if (appType != "standalone" && channel != "production" && channel != "qa")
+    return appType === "ios" ? "WTIwMWNHSXpUblpqUkc5M1RHcEJkVTFFY0doalIwWjNaRmMwUFE9PQ==" : "WTIwMWFHSnRVbmxpTW14cllqTkJOazFETkhkTWFrRTJXVmhDYUdOSVZuVT0=";
+
+    // Production
+    let app = appType + "|" + appVersion;
+    switch (app) {
+        case "android|0.1.2": console.error(clientGuide);
+        case "ios|0.1.2": console.error(clientGuide);
+
+        default: console.error(clientGuide);
+    }
+})();
+
+export const clientSecret = (() => {
+    // DEV
+    if (appType != "standalone" && channel != "production" && channel != "qa")
+        return appType === "ios" ? "StandariOS" : "StandarAndroid";
+
+    // Production
+    let app = appType + "|" + appVersion;
+    switch (app) {
+        case "android|0.1.2": console.error(clientGuide);
+        case "ios|0.1.2": console.error(clientGuide);
+
+        default: console.error(clientGuide);
+    }
+})();
--- screens/EditDetailActivity.js
+++ screens/EditDetailActivity.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { WebView, TouchableOpacity } from 'react-native';
 import { clientId, clientSecret } from '../constants/env';
 import { NavigationActions } from 'react-navigation';
-import { DOMAIN } from '../constants/env';
+import { WEB_DOMAIN } from '../constants/env';
 import { Icon } from 'react-native-elements';
 
 export default class EditDetailActivity extends Component {
@@ -19,8 +19,8 @@ export default class EditDetailActivity extends Component {
       <WebView
         startInLoadingState={true}
         source={{
-          uri: DOMAIN + '/id/webview/redirectcontact?text=' + text,
-          // uri: DOMAIN + '/id/payment/cartcheckout',
+          uri: WEB_DOMAIN + '/id/webview/redirectcontact?text=' + text,
+          // uri: WEB_DOMAIN + '/id/payment/cartcheckout',
           // '/id/payment/payment?rsvno=' + rsvNo +
           // '&regid=' + encodeURIComponent(Base64.stringify( SHA1(rsvNo) )),
           headers: {
